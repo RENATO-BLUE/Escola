ee775bb471c4c966fcb3935e0d575a1cfa4e9ad2
RENATO SANTOS PEREIRA <engenheirorenatoes@gmail.com>
2022-01-03 12:55:37 -0300

    adicionar revalidaçã2w

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,17 +1,21 @@
 function Tempo(props) {
+    console.log('>Passando pelo frontend;')
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
 
     return (
         <div>
             <div>{dynamicDateString} (dinâmico)</div>
-            <div>{props.staticDateString} (estático)</div>
+            <div>{props.staticDateString} (estático - com delay)</div>
         </div>
     )
 
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+    console.log('>Passando pelo getStaticProps();');
+    console.log('>Adicionando delay de 5 segundos');
+    await DelayNode(5000);
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
     return {
