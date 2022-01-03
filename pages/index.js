af616c3f58f7e4fc3397603dd4ca327c853c1c10
RENATO SANTOS PEREIRA <engenheirorenatoes@gmail.com>
2022-01-03 11:41:42 -0300

    DEUS É +

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
-function Home(){
-<<<<<<< HEAD
+function Home() {
     return <div><h1>Home vai tomar no seu cú que deu certo</h1></div>
-=======
-    return <div><h1>Home teste rena</h1></div>
->>>>>>> remotes/origin/main
+
 }
 
 export default Home
